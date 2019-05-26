e0e236113ea93d880f02302e062dd4d1aa8f5c8e
roechsli <romanoech@gmail.com>
2019-05-26 13:19:52 +0200

    clean and refactor code

--- searchImage/check_puzzle_matchtemplate.py
+++ searchImage/check_puzzle_matchtemplate.py
@@ -1,14 +1,16 @@
 import cv2
 import os
-import numpy as np
 import collections
 from matplotlib import pyplot as plt
 PLOT = True
+VERBOSE = True
+
 
 def show_image(image):
     cv2.imshow('image', image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
 
 def scaled_image(image, pix_size):
     w, h = image.shape[::-1]
@@ -17,7 +19,8 @@ def scaled_image(image, pix_size):
         w, h = int(w/z), int(h/z)
         return cv2.resize(image, (w, h), interpolation=cv2.INTER_AREA)
 
-def search_within_folders(target_image, result_vec):
+
+def search_within_folders(target_image, result_vec, global_min_val):
     for folder in os.listdir(targetpath):
         print("In folder ", folder)
         for picture in os.listdir(targetpath + folder):
@@ -26,9 +29,9 @@ def search_within_folders(target_image, result_vec):
             if folder not in ('1', '103', '47', '165'):
                 continue
             img = cv2.imread(targetpath + folder + "/" + picture, 0)
-            wt, ht = target_image.shape[::-1]
-            #img = cv2.Canny(img, wt, ht)
-            #show_image(img)
+            # wt, ht = target_image.shape[::-1]
+            # img = cv2.Canny(img, wt, ht)
+            # show_image(img)
 
             # Apply template Matching
             try:
@@ -37,94 +40,71 @@ def search_within_folders(target_image, result_vec):
                 continue
 
             min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-            #print(min_val)
-            if min_val < result_vec["global_min_val"]:
-                result_vec["global_min_val"] = min_val
-                print("folder = ", folder)
-                print("path = ", targetpath + folder + "/" + picture)
-                result_vec[folder]["path"] = targetpath + folder + "/" + picture
-                result_vec[folder]["min_val"] = min_val
-                if "count" in result_vec[folder].keys():
-                    result_vec[folder]["count"] += 1
+            if min_val < global_min_val:
+                global_min_val = min_val
+                if VERBOSE:
+                    print("folder = ", folder)
+                    print("path = ", targetpath + folder + "/" + picture)
+                result = collections.defaultdict(dict)#{}
+                result["folder"] = folder
+                result["path"] = targetpath + folder + "/" + picture
+                result["min_val"] = min_val
+                result["location"] = min_loc
+                if folder in (entry["folder"] for entry in result_vec):
+                    pass
                 else:
-                    result_vec[folder]["count"] = 1
-                    print("found new one")
-                result_vec[folder]["location"] = min_loc
-    return result_vec
+                    result["count"] = 1
+                    result_vec.append(result)
+                for entry in result_vec:
+                    if folder in entry["folder"]:
+                        entry["count"] += 1
+    return result_vec, global_min_val
+
 
 targetpath = '../get_images/Pictures/'
 puzzlepath = '../puzzle3.jpg'
-#puzzlepath = '../coverraetsel_1705.jpg'
+# puzzlepath = '../coverraetsel_1705.jpg'
 pixel_size_vec = [50] # np.linspace(50, 50, (150-30)/5)
 puzzle = cv2.imread(puzzlepath, 0)
 method = eval('cv2.TM_SQDIFF_NORMED')
 
 
-#puzzle = cv2.Canny(puzzle, w, h)
+# puzzle = cv2.Canny(puzzle, w, h)
 show_image(puzzle)
-result_vec = collections.defaultdict(dict)
-result_vec["global_min_val"] = 100
+result_vec = []
+global_min_val = 100
 
 
-"""
-result_vec = {'global_min_val': 0.08379919081926346,
-              '1': {'path': '../get_images/Pictures/1/11.jpg', 'min_val': 0.11378612369298935, 'count': 2, 'location': (515, 769)},
-              '10': {'path': '../get_images/Pictures/10/101.jpg', 'min_val': 0.09999868273735046, 'count': 2, 'location': (322, 278)},
-              '104': {'path': '../get_images/Pictures/104/1040.jpg', 'min_val': 0.08929844200611115, 'count': 1, 'location': (343, 436)},
-              '183': {'path': '../get_images/Pictures/183/1830.jpg', 'min_val': 0.08782242983579636, 'count': 1, 'location': (272, 794)},
-              '51': {'path': '../get_images/Pictures/51/511.jpg', 'min_val': 0.08379919081926346, 'count': 1, 'location': (134, 78)}}
-for key in result_vec.keys():
-    try:
-        print(result_vec[key]['path'])
-        #print(result_vec[key].keys())
-    except:
-        print("ignoring ", key)
-for key, value in result_vec.items():
-    if key == "path":
-        print(value)
-        
-        
-solution:
-defaultdict(<class 'dict'>, {'global_min_val': 0.08379919081926346, 
-'1': {'path': '../get_images/Pictures/1/11.jpg', 'min_val': 0.11378612369298935, 'count': 2, 'location': (515, 769)}, 
-'10': {'path': '../get_images/Pictures/10/101.jpg', 'min_val': 0.09999868273735046, 'count': 2, 'location': (322, 278)}, 
-'104': {'path': '../get_images/Pictures/104/1040.jpg', 'min_val': 0.08929844200611115, 'count': 1, 'location': (343, 436)}, 
-'183': {'path': '../get_images/Pictures/183/1830.jpg', 'min_val': 0.08782242983579636, 'count': 1, 'location': (272, 794)}, 
-'51': {'path': '../get_images/Pictures/51/511.jpg', 'min_val': 0.08379919081926346, 'count': 1, 'location': (134, 78)}})
-
-
-"""
 for pixel_size in pixel_size_vec:
     puzzle_scaled = scaled_image(puzzle, pixel_size)
     print("pixel_size: ", pixel_size)
-    result_vec = search_within_folders(puzzle_scaled, result_vec)
+    result_vec, global_min_val = search_within_folders(puzzle_scaled, result_vec, global_min_val)
 
-print(result_vec)
-for key in result_vec.keys():
-    print(key)
-    print(result_vec[key]["min_val"])
-    print(result_vec["global_min_val"])
-    #if result_vec[key]["min_val"] != result_vec["global_min_val"]:
-    #    continue
+if VERBOSE:
+    print(result_vec)
+for guess in [result_vec[-1]]:# or ::-1
+    print("guess = ", guess)
+    print(guess["min_val"])
     try:
-        candidate_path = result_vec[key]['path']
-        # print(result_vec[key].keys())
+        candidate_path = guess["path"]
         if PLOT:
             img = cv2.imread(candidate_path, 0)
-            wt, ht = puzzle.shape[::-1]
+            wt, ht = puzzle_scaled.shape[::-1]
+            print("wt = ", wt, ht)
             # w_img, h_img = img.shape[::-1]
             # img = cv2.Canny(img, w_img, h_img)
             try:
-                res = cv2.matchTemplate(img, puzzle, method)
+                res = cv2.matchTemplate(img, puzzle_scaled, method)
             except:
                 continue
             min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
             top_left = min_loc
+            print("min loc = ", min_loc)
             bottom_right = (top_left[0] + wt, top_left[1] + ht)
 
             cv2.rectangle(img, top_left, bottom_right, 255, 2)
 
-            plt.subplot(121), plt.imshow(puzzle, cmap='gray')
+            plt.subplot(121), plt.imshow(puzzle_scaled, cmap='gray')
             plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
             plt.subplot(122), plt.imshow(img, cmap='gray')
             plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
@@ -133,4 +113,5 @@ for key in result_vec.keys():
             plt.show()
 
     except:
-        print("ignoring ", key)
+        print("ignoring ", guess["folder"])
+
